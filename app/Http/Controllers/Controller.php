5382cafe0fa428353a659090bb284d97499af15f
muhammad alif alferdiansyah <alifalferdiansyah7@gmail.com>
2024-08-02 09:16:10 +0700

    initial config

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,23 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+class ChartController extends Controller
 {
-    //
-}
+    public function renderChart()
+    {
+        $path = storage_path('app/public/DATA_JULI_2024.xlsx');
+        $data = $this->readExcel($path);
+
+        // Logika untuk membuat grafik dengan data
+        return view('chart', ['data' => $data]);
+    }
+
+    private function readExcel($path)
+    {
+        $spreadsheet = IOFactory::load($path);
+        $worksheet = $spreadsheet->getActiveSheet();
+        $rows = [];
+        foreach
+        
