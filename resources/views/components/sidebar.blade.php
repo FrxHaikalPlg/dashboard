2204ae77bf5b375addd1d3fa6e134ce81778fc64
rexxar280903 <rexxar280903@gmail.com>
2024-08-05 14:58:29 +0700

    tombol input

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -53,6 +53,11 @@
                    </li>
              </ul>
           </li>
+          <li>
+             <button data-modal-target="popup-modal" data-modal-toggle="popup-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+                Toggle modal
+             </button>
+          </li>
        </ul>
     </div>
 </aside>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,6 +70,27 @@
    <div id="barDataCounts" class="hidden">@json($barDataCounts)</div>
    <div id="barDataLabels" class="hidden">@json($barDataLabels)</div>
 
+
+
+   <!-- Modal -->
+   <div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+      <div class="relative p-4 w-full max-w-md max-h-full">
+         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
+               <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+               </svg>
+               <span class="sr-only">Close modal</span>
+            </button>
+
+            <div class="p-4 md:p-5 text-center">
+               <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
+               <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
+            </div>
+         </div>
+      </div>
+   </div>
+
    <script src="/js/dark-mode.js"></script>
    <script src="/js/chart.js"></script>
    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
